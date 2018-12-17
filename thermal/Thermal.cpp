24442f416a7fd2b271247c76b17783b06bcf523f
Xin Li <delphij@google.com>
2018-12-17 11:03:18 -0800

    DO NOT MERGE: A new start for pi-dev
    Bug: 120303198

---
+++ thermal/Thermal.cpp
@@ -0,0 +1,189 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cerrno>
+#include <string>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+
+#include "Thermal.h"
+#include "thermal-helper.h"
+
+namespace android {
+namespace hardware {
+namespace thermal {
+namespace V1_0 {
+namespace implementation {
+
+namespace {
+
+using ::android::hardware::hidl_death_recipient;
+using ::android::hardware::thermal::V1_0::CoolingDevice;
+using ::android::hardware::thermal::V1_0::Temperature;
+using ::android::hardware::thermal::V1_0::ThermalStatus;
+using ::android::hardware::thermal::V1_0::ThermalStatusCode;
+using ::android::hardware::Void;
+using ::android::hidl::base::V1_0::IBase;
+using ::android::wp;
+
+template <typename T, typename U>
+Return<void> setFailureAndCallback(
+    T _hidl_cb, hidl_vec<U> data, const std::string& debug_msg) {
+    ThermalStatus status;
+    status.code = ThermalStatusCode::FAILURE;
+    status.debugMessage = debug_msg;
+    _hidl_cb(status, data);
+    return Void();
+}
+
+template <typename T, typename U>
+Return<void> setInitFailureAndCallback(T _hidl_cb, hidl_vec<U> data) {
+    return setFailureAndCallback(
+        _hidl_cb, data, "Failure initializing thermal HAL");
+}
+
+}  // namespace
+
+void Thermal::serviceDied(
+        uint64_t cookie __unused, const wp<IBase>& who __unused) {
+    LOG(ERROR) << "IThermalCallback HIDL service died";
+}
+
+Thermal::Thermal() : thermal_helper_() {
+}
+
+// Methods from ::android::hardware::thermal::V1_0::IThermal.
+Return<void> Thermal::getTemperatures(getTemperatures_cb _hidl_cb) {
+    ThermalStatus status;
+    status.code = ThermalStatusCode::SUCCESS;
+    hidl_vec<Temperature> temperatures;
+
+    if (!thermal_helper_.isInitializedOk()) {
+        LOG(ERROR) << "ThermalHAL not initialized properly.";
+        return setInitFailureAndCallback(_hidl_cb, temperatures);
+    }
+
+    if (!thermal_helper_.fillTemperatures(&temperatures)) {
+        return setFailureAndCallback(_hidl_cb, temperatures,
+                "Failed to read thermal sensors.");
+    }
+
+    for (const auto& t : temperatures) {
+        LOG(DEBUG) << "getTemperatures "
+                   << " Type: " << android::hardware::thermal::V1_0::toString(t.type)
+                   << " Name: " << t.name
+                   << " CurrentValue: " << t.currentValue
+                   << " ThrottlingThreshold: " << t.throttlingThreshold
+                   << " ShutdownThreshold: " << t.shutdownThreshold
+                   << " VrThrottlingThreshold: " << t.vrThrottlingThreshold;
+    }
+    _hidl_cb(status, temperatures);
+    return Void();
+}
+
+Return<void> Thermal::getCpuUsages(getCpuUsages_cb _hidl_cb) {
+    ThermalStatus status;
+    status.code = ThermalStatusCode::SUCCESS;
+    hidl_vec<CpuUsage> cpu_usages;
+
+    if (!thermal_helper_.isInitializedOk()) {
+        return setInitFailureAndCallback(_hidl_cb, cpu_usages);
+    }
+
+    if (!thermal_helper_.fillCpuUsages(&cpu_usages)) {
+        return setFailureAndCallback(_hidl_cb, cpu_usages,
+            "Failed to get CPU usages.");
+    }
+
+    for (const auto& usage : cpu_usages) {
+        LOG(DEBUG) << "getCpuUsages "
+                   << " Name: " << usage.name
+                   << " Active: " << usage.active
+                   << " Total: " << usage.total
+                   << " IsOnline: " << usage.isOnline;
+    }
+    _hidl_cb(status, cpu_usages);
+    return Void();
+}
+
+Return<void> Thermal::getCoolingDevices(getCoolingDevices_cb _hidl_cb) {
+    ThermalStatus status;
+    status.code = ThermalStatusCode::SUCCESS;
+    hidl_vec<CoolingDevice> cooling_devices;
+
+    if (!thermal_helper_.isInitializedOk()) {
+        return setInitFailureAndCallback(_hidl_cb, cooling_devices);
+    }
+    LOG(DEBUG) << "No cooling device.";
+    _hidl_cb(status, cooling_devices);
+    return Void();
+}
+
+Return<void> Thermal::debug(
+        const hidl_handle& handle, const hidl_vec<hidl_string>&) {
+    if (handle != nullptr && handle->numFds >= 1) {
+        int fd = handle->data[0];
+        std::ostringstream dump_buf;
+
+        if (!thermal_helper_.isInitializedOk()) {
+            dump_buf << "ThermalHAL not initialized properly." << std::endl;
+        } else {
+            hidl_vec<Temperature> temperatures;
+            hidl_vec<CpuUsage> cpu_usages;
+
+            dump_buf << "getTemperatures:" << std::endl;
+            if (!thermal_helper_.fillTemperatures(&temperatures)) {
+                dump_buf << "Failed to read thermal sensors." << std::endl;
+            }
+
+            for (const auto& t : temperatures) {
+                dump_buf << "Name: " << t.name << " Type: "
+                         << android::hardware::thermal::V1_0::toString(t.type)
+                         << " CurrentValue: " << t.currentValue
+                         << " ThrottlingThreshold: " << t.throttlingThreshold
+                         << " ShutdownThreshold: " << t.shutdownThreshold
+                         << " VrThrottlingThreshold: "
+                         << t.vrThrottlingThreshold << std::endl;
+            }
+
+            dump_buf << "getCpuUsages:" << std::endl;
+            if (!thermal_helper_.fillCpuUsages(&cpu_usages)) {
+                dump_buf << "Failed to get CPU usages." << std::endl;
+            }
+
+            for (const auto& usage : cpu_usages) {
+                dump_buf << "Name: " << usage.name
+                         << " Active: " << usage.active
+                         << " Total: " << usage.total
+                         << " IsOnline: " << usage.isOnline
+                         << std::endl;
+            }
+        }
+        std::string buf = dump_buf.str();
+        if (!android::base::WriteStringToFd(buf, fd)) {
+            PLOG(ERROR) << "Failed to dump state to fd";
+        }
+        fsync(fd);
+    }
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace thermal
+}  // namespace hardware
+}  // namespace android
---
+++ vibrator/service.cpp
@@ -0,0 +1,202 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#define LOG_TAG "android.hardware.vibrator@1.2-service.coral"
+
+#include <android/hardware/vibrator/1.2/IVibrator.h>
+#include <hidl/HidlSupport.h>
+#include <hidl/HidlTransportSupport.h>
+#include <utils/Errors.h>
+#include <utils/StrongPointer.h>
+
+#include "Vibrator.h"
+
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+using android::hardware::vibrator::V1_2::IVibrator;
+using android::hardware::vibrator::V1_2::implementation::Vibrator;
+using namespace android;
+
+static constexpr char ACTIVATE_PATH[] = "/sys/class/leds/vibrator/activate";
+static constexpr char DURATION_PATH[] = "/sys/class/leds/vibrator/duration";
+static constexpr char STATE_PATH[] = "/sys/class/leds/vibrator/state";
+static constexpr char EFFECT_INDEX_PATH[] = "/sys/class/leds/vibrator/device/cp_trigger_index";
+static constexpr char EFFECT_QUEUE_PATH[] = "/sys/class/leds/vibrator/device/cp_trigger_queue";
+static constexpr char DIGI_SCALE_PATH[] = "/sys/class/leds/vibrator/device/dig_scale";
+
+// File path to the calibration file
+static constexpr char CALIBRATION_FILEPATH[] = "/persist/haptics/cs40l25a.cal";
+
+// Kernel ABIs for updating the calibration data
+static constexpr char F0_CONFIG[] = "f0_measured";
+static constexpr char REDC_CONFIG[] = "redc_measured";
+static constexpr char BEST_Q_CONFIG[] = "q_index";
+static constexpr char VOLTAGES_CONFIG[] = "v_levels";
+static constexpr char F0_FILEPATH[] = "/sys/class/leds/vibrator/device/f0_stored";
+static constexpr char REDC_FILEPATH[] = "/sys/class/leds/vibrator/device/redc_stored";
+static constexpr char BEST_Q_FILEPATH[] = "/sys/class/leds/vibrator/device/q_index";
+
+static std::string trim(const std::string& str,
+        const std::string& whitespace = " \t") {
+    const auto str_begin = str.find_first_not_of(whitespace);
+    if (str_begin == std::string::npos) {
+        return "";
+    }
+
+    const auto str_end = str.find_last_not_of(whitespace);
+    const auto str_range = str_end - str_begin + 1;
+
+    return str.substr(str_begin, str_range);
+}
+
+static void ltrim(std::istream& stream) {
+    while (std::isspace(static_cast<char>(stream.get()))) { }
+    stream.unget();
+}
+
+static bool loadCalibrationData(std::uint32_t& q_index, std::vector<uint32_t>& v_levels) {
+    std::map<std::string, std::string> config_data;
+
+    std::ofstream f0{F0_FILEPATH};
+    if (!f0) {
+        ALOGE("Failed to open %s (%d): %s", F0_FILEPATH, errno,
+                strerror(errno));
+    }
+
+    std::ofstream redc{REDC_FILEPATH};
+    if (!redc) {
+        ALOGE("Failed to open %s (%d): %s", REDC_FILEPATH, errno,
+                strerror(errno));
+    }
+
+    std::ofstream bestQ{BEST_Q_FILEPATH};
+    if (!bestQ) {
+        ALOGE("Failed to open %s (%d): %s", BEST_Q_FILEPATH, errno,
+                strerror(errno));
+    }
+
+    std::ifstream cal_data{CALIBRATION_FILEPATH};
+    if (!cal_data) {
+        ALOGE("Failed to open %s (%d): %s", CALIBRATION_FILEPATH, errno,
+                strerror(errno));
+        return false;
+    }
+
+    for (std::string line; std::getline(cal_data, line);) {
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+        std::istringstream is_line(line);
+        std::string key, value;
+        if (std::getline(is_line, key, ':')) {
+            uint32_t i = 0;
+            if (key == VOLTAGES_CONFIG) {
+                ltrim(is_line);
+                while (std::getline(is_line, value, ' ')) {
+                    if (i >= v_levels.size()) {
+                        ALOGE("invalid v_levels config!");
+                        return false;
+                    }
+                    v_levels[i++] = std::stoul(value);
+                }
+                if (i != v_levels.size()) {
+                        ALOGE("invalid v_levels config!");
+                        return false;
+                }
+                continue;
+            }
+            else if (std::getline(is_line, value)) {
+                config_data[trim(key)] = trim(value);
+            }
+        }
+    }
+
+    if(config_data.find(F0_CONFIG) != config_data.end()) {
+        f0 << config_data[F0_CONFIG] << std::endl;
+    }
+
+    if(config_data.find(REDC_CONFIG) != config_data.end()) {
+        redc << config_data[REDC_CONFIG] << std::endl;
+    }
+
+    if(config_data.find(BEST_Q_CONFIG) != config_data.end()) {
+        bestQ << config_data[BEST_Q_CONFIG] << std::endl;
+        q_index = std::stoul(config_data[BEST_Q_CONFIG]);
+    }
+
+    return true;
+}
+
+status_t registerVibratorService() {
+    // Calibration data
+    std::uint32_t q_index(9);
+    std::vector<uint32_t> v_levels= {60, 70, 80, 90, 100, 76};
+
+    // ostreams below are required
+    std::ofstream activate{ACTIVATE_PATH};
+    if (!activate) {
+        ALOGE("Failed to open %s (%d): %s", ACTIVATE_PATH, errno, strerror(errno));
+    }
+
+    std::ofstream duration{DURATION_PATH};
+    if (!duration) {
+        ALOGE("Failed to open %s (%d): %s", DURATION_PATH, errno, strerror(errno));
+    }
+
+    std::ofstream state{STATE_PATH};
+    if (!state) {
+        ALOGE("Failed to open %s (%d): %s", STATE_PATH, errno, strerror(errno));
+    }
+
+    std::ofstream effect{EFFECT_INDEX_PATH};
+    if (!state) {
+        ALOGE("Failed to open %s (%d): %s", EFFECT_INDEX_PATH, errno, strerror(errno));
+    }
+
+    std::ofstream queue{EFFECT_QUEUE_PATH};
+    if (!state) {
+        ALOGE("Failed to open %s (%d): %s", EFFECT_QUEUE_PATH, errno, strerror(errno));
+    }
+
+    std::ofstream scale{DIGI_SCALE_PATH};
+    if (!scale) {
+        ALOGE("Failed to open %s (%d): %s", DIGI_SCALE_PATH, errno, strerror(errno));
+    }
+
+    state << 1 << std::endl;
+    if (!state) {
+        ALOGE("Failed to set state (%d): %s", errno, strerror(errno));
+    }
+
+    if (!loadCalibrationData(q_index, v_levels)) {
+        ALOGW("Failed to load calibration data");
+    }
+
+    sp<IVibrator> vibrator = new Vibrator(std::move(activate), std::move(duration),
+        std::move(effect), std::move(queue), std::move(scale), q_index, std::move(v_levels));
+
+    return vibrator->registerAsService();
+}
+
+int main() {
+    configureRpcThreadpool(1, true);
+    status_t status = registerVibratorService();
+
+    if (status != OK) {
+        return status;
+    }
+
+    joinRpcThreadpool();
+}
