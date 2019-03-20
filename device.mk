2e40dfa104b14e52288285648f71869fea97df29
Jenhao Chen <jenhaochen@google.com>
2019-03-20 14:57:51 -0700

    Enable zram writeback
    Bug: 128889899
    Test: Boot to home
    
    Change-Id: I48ae06e54a66a7d6965afbe8db3d2f157cf8a1f4

--- device.mk
+++ device.mk
@@ -843,3 +843,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+# ZRAM writeback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
+
