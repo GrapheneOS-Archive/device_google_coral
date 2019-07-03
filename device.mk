83590d99d9e36e313ac75bde8d8921bb35ae80a0
Wonsik Kim <wonsik@google.com>
2019-07-03 15:54:30 -0700

    Use the right media_codecs_performance.xml
    Add missing performance data as well.
    
    Bug: 132940025
    Test: atest CtsMediaTestCases:VideoDecoderPerfTest
    Change-Id: I40638983c1cf3d2005f64d60d31c2325009924b5

--- device.mk
+++ device.mk
@@ -432,10 +432,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qc2.venc.avgqp.enable=1
 
-# TODO: remove once we generate ours
-PRODUCT_PACKAGES += \
-    media_codecs_performance_c2.xml
-
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-google \
     android.hardware.camera.provider@2.4-service-google \
@@ -682,6 +678,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media_codecs_omx.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_omx.xml \
+    $(LOCAL_PATH)/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
