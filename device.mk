acbdd6ef6a6fbf2143a877e9d946c2d42bc84771
Steven Moreland <smoreland@google.com>
2019-11-06 18:17:25 -0800

    Use AIDL vibrator cs40l25 impl.
    Since it is now using AIDL, not HIDL.
    
    Bug: 141828236
    Test: boot, and use vibrator
    Change-Id: I8acb7ad84def7eab538b21a102af9b855fbf83c0

--- device.mk
+++ device.mk
@@ -482,7 +482,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator HAL
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.4-service.cs40l25 \
+    android.hardware.vibrator-service.cs40l25 \
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
