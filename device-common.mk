56dda22acf440592f047b643e61b26e4dcb8f6e2
Ady Abraham <adyabr@google.com>
2020-02-04 16:08:53 -0800

    coral: rename use_smart_90_for_video flag
    Rename use_smart_90_for_video -> use_content_detection_for_refresh_rate
    to better describe what it controls.
    
    Test: Play video and observe the refresh rate
    Change-Id: I5ae03073bd670d0b3bc7f2861cbf3ec0d28efbe7

--- device-common.mk
+++ device-common.mk
@@ -95,7 +95,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_idle_timer_ms=80
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_touch_timer_ms=200
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_display_power_timer_ms=1000
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.support_kernel_idle_timer=true
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.use_smart_90_for_video=true
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.use_content_detection_for_refresh_rate=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.refresh_rate_switching=true
 
