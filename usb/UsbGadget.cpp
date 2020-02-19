TeamWin/android_device_google_crosshatch

Update USB Gadget HAL to V1.1 implementation
Bug: 138702846
Test: build pass, function works
Change-Id: I83fe2e0d90e809d5a5a3a94abcde0d3ebb3989e4

usb/UsbGadget.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.usb.gadget@1.0-service.crosshatch"
+#define LOG_TAG "android.hardware.usb.gadget@1.1-service.crosshatch"
 
 #include "UsbGadget.h"
 #include <dirent.h>
@@ -58,7 +58,7 @@ namespace android {
 namespace hardware {
 namespace usb {
 namespace gadget {
-namespace V1_0 {
+namespace V1_1 {
 namespace implementation {
 
 volatile bool gadgetPullup;
@@ -281,6 +281,15 @@ V1_0::Status UsbGadget::tearDownGadget() {
   mEpollFd.reset(-1);
   mEndpointList.clear();
   return Status::SUCCESS;
+}
+
+Return<Status> UsbGadget::reset() {
+    if (!WriteStringToFile("none", PULLUP_PATH)) {
+        ALOGI("Gadget cannot be pulled down");
+        return Status::ERROR;
+    }
+
+    return Status::SUCCESS;
 }
 
 static int linkFunction(const char *function, int index) {
@@ -670,7 +679,7 @@ error:
   return Void();
 }
 }  // namespace implementation
-}  // namespace V1_0
+}  // namespace V1_1
 }  // namespace gadget
 }  // namespace usb
 }  // namespace hardware

GrapheneOS-Archive/device_google_coral

Change-Id: I82a74d31f4339e4b4b8c59d879871550b943a36e

usb/UsbGadget.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.usb.gadget@1.0-service.coral"
+#define LOG_TAG "android.hardware.usb.gadget@1.1-service.coral"
 
 #include "UsbGadget.h"
 #include <dirent.h>
@@ -30,7 +30,7 @@ namespace android {
 namespace hardware {
 namespace usb {
 namespace gadget {
-namespace V1_0 {
+namespace V1_1 {
 namespace implementation {
 
 UsbGadget::UsbGadget() {
@@ -216,6 +216,15 @@ static V1_0::Status validateAndSetVidPid(uint64_t functions) {
     return ret;
 }
 
+Return<Status> UsbGadget::reset() {
+    if (!WriteStringToFile("none", PULLUP_PATH)) {
+        ALOGI("Gadget cannot be pulled down");
+        return Status::ERROR;
+    }
+
+    return Status::SUCCESS;
+}
+
 V1_0::Status UsbGadget::setupFunctions(uint64_t functions,
                                        const sp<V1_0::IUsbGadgetCallback> &callback,
                                        uint64_t timeout) {
@@ -337,7 +346,7 @@ error:
     return Void();
 }
 }  // namespace implementation
-}  // namespace V1_0
+}  // namespace V1_1
 }  // namespace gadget
 }  // namespace usb
 }  // namespace hardware