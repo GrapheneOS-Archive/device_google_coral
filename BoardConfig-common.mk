d5a0603177939d2de62c5cb50d50ac2fb23ffb2c
Dmitry Dmitriev <dmitriev.dd@hotmail.com>
2020-06-04 22:16:56 +0300

    move to monolithic kernel builds

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -216,37 +216,6 @@ DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE := device/google/coral/device_framework
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
 
-# Kernel modules
-ifeq (,$(filter-out flame_kasan coral_kasan, $(TARGET_PRODUCT)))
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(wildcard device/google/coral-kernel/kasan/*.ko)
-else ifeq (,$(filter-out flame_hwasan coral_hwasan, $(TARGET_PRODUCT)))
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(wildcard device/google/coral-kernel/khwasan/*.ko)
-else ifeq (,$(filter-out flame_boundsan coral_boundsan, $(TARGET_PRODUCT)))
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(wildcard device/google/coral-kernel/boundsan/*.ko)
-else ifeq (,$(filter-out flame_kernel_debug_memory coral_kernel_debug_memory, $(TARGET_PRODUCT)))
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(wildcard device/google/coral-kernel/debug_memory/*.ko)
-else ifeq (,$(filter-out flame_kernel_debug_memory_accounting coral_kernel_debug_memory_accounting, $(TARGET_PRODUCT)))
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(wildcard device/google/coral-kernel/debug_memory_accounting/*.ko)
-BOARD_KERNEL_CMDLINE += page_owner=on
-else ifeq (,$(filter-out flame_kernel_debug_locking coral_kernel_debug_locking, $(TARGET_PRODUCT)))
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(wildcard device/google/coral-kernel/debug_locking/*.ko)
-else ifeq (,$(filter-out flame_kernel_debug_hang coral_kernel_debug_hang, $(TARGET_PRODUCT)))
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(wildcard device/google/coral-kernel/debug_hang/*.ko)
-else ifeq (,$(filter-out flame_kernel_debug_api coral_kernel_debug_api, $(TARGET_PRODUCT)))
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(wildcard device/google/coral-kernel/debug_api/*.ko)
-else
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(wildcard device/google/coral-kernel/*.ko)
-endif
-
 BOARD_SUPER_PARTITION_SIZE := 9755951104
 BOARD_SUPER_PARTITION_GROUPS := google_dynamic_partitions
 BOARD_GOOGLE_DYNAMIC_PARTITIONS_PARTITION_LIST := \
@@ -261,22 +230,7 @@ BOARD_GOOGLE_DYNAMIC_PARTITIONS_SIZE := 4873781248
 # Set error limit to BOARD_SUPER_PARTITON_SIZE - 100MB
 BOARD_SUPER_PARTITION_ERROR_LIMIT := 9651093504
 
-# DTB
-ifeq (,$(filter-out coral_kasan flame_kasan, $(TARGET_PRODUCT)))
-BOARD_PREBUILT_DTBIMAGE_DIR := device/google/coral-kernel/kasan
-else ifeq (,$(filter-out flame_kernel_debug_memory coral_kernel_debug_memory, $(TARGET_PRODUCT)))
-BOARD_PREBUILT_DTBIMAGE_DIR := device/google/coral-kernel/debug_memory
-else ifeq (,$(filter-out flame_kernel_debug_memory_accounting coral_kernel_debug_memory_accounting, $(TARGET_PRODUCT)))
-BOARD_PREBUILT_DTBIMAGE_DIR := device/google/coral-kernel/debug_memory_accounting
-else ifeq (,$(filter-out flame_kernel_debug_locking coral_kernel_debug_locking, $(TARGET_PRODUCT)))
-BOARD_PREBUILT_DTBIMAGE_DIR := device/google/coral-kernel/debug_locking
-else ifeq (,$(filter-out flame_kernel_debug_hang coral_kernel_debug_hang, $(TARGET_PRODUCT)))
-BOARD_PREBUILT_DTBIMAGE_DIR := device/google/coral-kernel/debug_hang
-else ifeq (,$(filter-out flame_kernel_debug_api coral_kernel_debug_api, $(TARGET_PRODUCT)))
-BOARD_PREBUILT_DTBIMAGE_DIR := device/google/coral-kernel/debug_api
-else
 BOARD_PREBUILT_DTBIMAGE_DIR := device/google/coral-kernel
-endif
 
 # Testing related defines
 BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/c2f2-setup.sh
