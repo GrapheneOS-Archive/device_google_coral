1822f1175dcdefd2ec2287eff9af690eaf87f788
Jason Chang <jasonsfchang@google.com>
2020-08-11 23:45:07 +0800

    Fine-tune the screen size by adb command to adjust the screen size for UX [Coral & Flame]
    Add system properties for UX adjust:
    1) one-handed screen translate offset percentage.
    2) one-handed screen translate animation duration milliseconds.
    
    [TODO]: This is temporary solution for UX to refine the threshold,
    and it needs to revert after the threshold has been confirmed.
    
    Bug: 153694010
    
    Test:
    adb shell setprop persist.debug.one_handed_offset_percentage 40
    adb shell pidof com.android.systemui |xargs -i adb shell kill -9 {}
    or adb reboot
    
    adb shell setprop persist.debug.one_handed_translate_animation_duration 150
    adb shell pidof com.android.systemui |xargs -i adb shell kill -9 {}
    or adb reboot
    
    Test: manual
    Test: atest SystemUITests
    Change-Id: I346bc640634d939bcacbf429c081da15b905e505

--- device.mk
+++ device.mk
@@ -1007,7 +1007,15 @@ endif
 
 # Set support one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_one_handed_mode = true
+    ro.support_one_handed_mode=true
+
+# Set one_handed_mode screen translate offset percentage
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.debug.one_handed_offset_percentage=50
+
+# Set one_handed_mode translate animation duration milliseconds
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.debug.one_handed_translate_animation_duration=300
 
 include hardware/google/pixel/common/pixel-common-device.mk
 include hardware/google/pixel/vibrator/cs40l25/device.mk
