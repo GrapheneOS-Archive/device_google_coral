59b82642a914d22a456c9efc665bff3ac9c52622
Yi Kong <yikong@google.com>
2020-09-17 09:58:04 +0800

    Populate ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST for profcollect flavours
    This ports corresponding allowed list entries from internal master.
    
    Fixes build breakage.
    
    Test: m nothing
    Bug: 168758229
    Change-Id: Ie9c62554ef89243e12382f3e282193e7b9b8f8d4

--- aosp_coral_profcollect.mk
+++ aosp_coral_profcollect.mk
@@ -22,3 +22,22 @@ PRODUCT_PACKAGES += profcollectctl profcollectd
 
 # Turn on PGO sampling cflags.
 SAMPLING_PGO := true
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/profcollectctl \
+    system/bin/profcollectd \
+    system/etc/init/profcollectd.rc \
+    system/lib64/libprofcollectd.so \
+    system/lib64/libsimpleperf_profcollect.so
+
+# TODO b/165792521
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/lib64/liblibprofcollectd.dylib.so \
+    system/lib64/libprofcollectd_bindgen.dylib.so
+
+# TODO remove these when mainline eventually includes these
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/lib64/libgetopts.dylib.so \
+    system/lib64/libstd.dylib.so \
+    system/lib64/libterm.dylib.so \
+    system/lib64/libtest.dylib.so
