9f11fd7e1dfc63faed34afaf0a7a324d94999446
Benjamin Schwartz <bsschwar@google.com>
2020-10-07 17:49:29 -0700

    powerstats: Add display energy consumer
    Bug: 170334134
    Test: dumpsys android.hardware.power.stats.IPowerStats/default
    Change-Id: Iea28e671e0323776098fe5c10aa366abd01f783d

--- powerstats/serviceaidl.cpp
+++ powerstats/serviceaidl.cpp
@@ -17,15 +17,47 @@
 #define LOG_TAG "android.hardware.power.stats-service.pixel"
 
 #include <PowerStatsAidl.h>
+#include <dataproviders/PowerStatsEnergyConsumer.h>
 #include <dataproviders/IioEnergyMeterDataProvider.h>
+#include <dataproviders/GenericStateResidencyDataProvider.h>
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
 #include <log/log.h>
 
+using aidl::android::hardware::power::stats::EnergyConsumerId;
 using aidl::android::hardware::power::stats::IioEnergyMeterDataProvider;
 using aidl::android::hardware::power::stats::PowerStats;
+using aidl::android::hardware::power::stats::PowerStatsEnergyConsumer;
+using aidl::android::hardware::power::stats::GenericStateResidencyDataProvider;
+
+void addNFCStats(std::shared_ptr<PowerStats> p) {
+    GenericStateResidencyDataProvider::StateResidencyConfig nfcStateConfig = {
+        .entryCountSupported = true,
+        .entryCountPrefix = "Cumulative count:",
+        .totalTimeSupported = true,
+        .totalTimePrefix = "Cumulative duration msec:",
+        .lastEntrySupported = true,
+        .lastEntryPrefix = "Last entry timestamp msec:"
+    };
+    std::vector<std::pair<std::string, std::string>> nfcStateHeaders = {
+        std::make_pair("Idle", "Idle mode:"),
+        std::make_pair("Active", "Active mode:"),
+        std::make_pair("Active-RW", "Active Reader/Writer mode:"),
+    };
+
+    std::vector<GenericStateResidencyDataProvider::PowerEntityConfig> cfgs;
+    cfgs.emplace_back(generateGenericStateResidencyConfigs(nfcStateConfig, nfcStateHeaders), "NFC");
+    android::sp<GenericStateResidencyDataProvider> nfcSdp = new GenericStateResidencyDataProvider(
+            "/sys/class/misc/st21nfc/device/power_stats", cfgs);
+    p->addStateResidencyDataProvider(nfcSdp);
+}
+
+void addDisplayEnergyConsumer(std::shared_ptr<PowerStats> p) {
+    p->addEnergyConsumer(PowerStatsEnergyConsumer::createMeterConsumer(p, EnergyConsumerId::DISPLAY,
+        {"PPVAR_VPH_PWR_OLED"}));
+}
 
 void setEnergyMeter(std::shared_ptr<PowerStats> p) {
     p->setEnergyMeterDataProvider(std::make_unique<IioEnergyMeterDataProvider>("microchip,pac1934"));
@@ -38,8 +70,13 @@ int main() {
     ABinderProcess_setThreadPoolMaxThreadCount(0);
 
     std::shared_ptr<PowerStats> p = ndk::SharedRefBase::make<PowerStats>();
+    if (!p) {
+        return EXIT_FAILURE;
+    }
 
     setEnergyMeter(p);
+    addNFCStats(p);
+    addDisplayEnergyConsumer(p); // Must be done after setEnergyMeter
 
     const std::string instance = std::string() + PowerStats::descriptor + "/default";
     binder_status_t status = AServiceManager_addService(p->asBinder().get(), instance.c_str());
