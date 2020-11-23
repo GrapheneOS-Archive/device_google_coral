fde91a467f12cd5636cc0ce69f03445823ce0cc9
Suren Baghdasaryan <surenb@google.com>
2020-11-23 11:43:41 -0800

    Revert "coral: Add vendor libprocessgroup json file"
    This reverts commit 579042e47ec235febf2a4d2ea13dd2e8efc91cae.
    
    Reason for revert: <with per-API profiles support no need for vendor overrides>
    
    Change-Id: I4f58f43f9e48a4b8a8e7f86503f867e5a0050039

--- device.mk
+++ device.mk
@@ -965,9 +965,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
-    $(LOCAL_PATH)/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
 
 # Disable SPU usage
 PRODUCT_PROPERTY_OVERRIDES += \
