4a2e359e61fd4a9d56b6157d9a864e0c6ab3d73f
Minchan Kim <minchan@google.com>
2021-01-06 13:01:47 -0800

    move dmabuf_tool into pixel common file
    Bug: 176859817
    Signed-off-by: Minchan Kim <minchan@google.com>
    Change-Id: I21b09902ebd5ff31bec1332e9100f6fe160b1d36

--- device-common.mk
+++ device-common.mk
@@ -111,10 +111,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml
 
-# Dmabuf dump tool for bug reports
-PRODUCT_PACKAGES += \
-    dmabuf_dump
-
 # Set thermal warm reset
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.thermal_warmreset = true \
