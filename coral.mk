be2979789afbb1d646ce4aebc3fea90f636d7bab
flawedworld <flawedworld@flawed.world>
2021-09-29 04:40:00 +0100

    Use APNs and CarrierConfig from APV

--- coral.mk
+++ coral.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/coral/aosp_coral.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := coral
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true
--- flame.mk
+++ flame.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/coral/aosp_flame.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := flame
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true
