4230a512c78abcc2a74eddd4c563fd479b4895a1
flawedworld <flawedworld@flawed.world>
2022-04-02 13:10:42 +0100

    Revert "remove aosp_ prefix from product names"
    This reverts commit 46690be9c44474edb35ecce3a1b7ed5e22c85df1.

--- coral.mk
+++
@@ -1,5 +0,0 @@
-# Inherit AOSP product configuration
-$(call inherit-product, device/google/coral/aosp_coral.mk)
-
-# Remove AOSP prefix from product name
-PRODUCT_NAME := coral
--- flame.mk
+++
@@ -1,5 +0,0 @@
-# Inherit AOSP product configuration
-$(call inherit-product, device/google/coral/aosp_flame.mk)
-
-# Remove AOSP prefix from product name
-PRODUCT_NAME := flame
