3b88c6988e55edc87b350920d087a4c9e9aaaeb1
flawedworld <flawedworld@flawed.world>
2022-04-13 20:03:37 +0100

    Add aggregated sysconfig xml with items from stock OS

--- device-common.mk
+++ device-common.mk
@@ -129,3 +129,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     extracted-carrierconfig \
     extracted-apns
+
+# sysconfig XML from stock
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
