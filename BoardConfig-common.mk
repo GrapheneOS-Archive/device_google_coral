991ffc4e3124daf7e9ca2860d39c9e9fee85a29b
flawedworld <flawedworld@flawed.world>
2022-06-07 14:11:15 +0100

    temp hack

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -33,6 +33,8 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
 
+BUILD_BROKEN_DUP_RULES := true
+
 TARGET_BOARD_COMMON_PATH := device/google/coral/sm8150
 
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on
