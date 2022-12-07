6c6b4754c3b1b8b682d0c42c08440690bd829689
flawedworld <flawedworld@flawed.world>
2022-12-07 20:05:03 +0000

    Add missing NFC libraries

--- device.mk
+++ device.mk
@@ -443,7 +443,11 @@ PRODUCT_PACKAGES += \
     Tag \
     SecureElement \
     android.hardware.nfc-service.st \
-    android.hardware.secure_element@1.0-service.st
+    android.hardware.secure_element@1.0-service.st \
+    android.hardware.nfc@1.0.vendor:64 \
+    android.hardware.nfc@1.1.vendor:64 \
+    android.hardware.nfc@1.2-service.st \
+    android.hardware.nfc@1.2.vendor:64
 
 PRODUCT_COPY_FILES += \
     device/google/coral/nfc/com.google.hardware.pixel.japan.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_G020N/com.google.hardware.pixel.japan.xml \
